43d5ef2a70cff081edb4008c15ae2d086bd3b4b6
akash-gogroup <akash.kumar@gogroup.co>
2024-08-05 11:46:06 +0530

    Added input

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,3 @@
 import './tailwind.css'
 export { Button } from './Button'
+export * from "./Input"
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { Button } from "gogroup-ui";
+import { Button, FloatingInput, Input } from "gogroup-ui";
 
 function App() {
   return (
     <>
       <div className="flex">
         <Button btnText="Hello" variant="secondary" />
+        <Input placeholder="username" />
+        <FloatingInput label="username" />
       </div>
     </>
   );
